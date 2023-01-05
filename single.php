7e0e5327d4d87c57054b53a83c0e1be8182d5e01
Tim <github@timgreen.ws>
2023-01-05 15:54:29 +1100

    added new function for older post - message

--- single.php
+++ single.php
@@ -22,6 +22,34 @@ get_header(); ?>
 		<main id="main" class="site-main">
 
 			<?php
+   				// $old_post = 60*60*24*365; // This is a year
+   				// if((date('U')-get_the_time('U')) > $old_post) {
+    			// 	echo '<div class="swc-old-notice">This post was last updated over a year ago, therefore the contents of this post may be out of date.</div>';
+    			// 	}
+			
+			
+			// Define old post duration to one year
+$time_defined_as_old = 60*60*24*365; 
+ 
+// Check to see if a post is older than a year
+if((date('U')-get_the_time('U')) > $time_defined_as_old) {
+ 
+$lastmodified = get_the_modified_time('U');
+$posted = get_the_time('U');
+ 
+//check if the post was updated after being published
+ if ($lastmodified > $posted) {
+  
+// Display last updated notice
+      echo '<div class="swc-old-notice"><i class="fa-regular fa-circle-exclamation"></i> This article was last updated ' . human_time_diff($lastmodified,current_time('U')) . ' ago, therefore the contents of this post could be out of date.</div>';   
+ 
+  } else { 
+// Display last published notice 
+echo '<div class="swc-old-notice"><i class="swc-fa fa-regular fa-circle-exclamation"></i> This article was published ' . human_time_diff($posted,current_time( 'U' )). ' ago, therefore the contents of this post may be out of date.</div>';
+ 
+}
+}
+
 				while ( have_posts() ) : the_post();
 
 					/**
