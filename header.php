e68009e5cd21c18b8f892dfb82717057cddea7d7
Tim <github@timgreen.ws>
2023-11-06 17:05:14 +1100

    clean up various files

--- header.php
+++ header.php
@@ -32,6 +32,8 @@
 </script>
 <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=317067237220408&ev=PageView&noscript=1"/></noscript>
 <!-- End Meta Pixel Code -->
+<!-- gads -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7556856852527309" crossorigin="anonymous"></script>
 
 </head>
 
--- includes/plugins/acf/acf-widget.php
+++ includes/plugins/acf/acf-widget.php
@@ -3,87 +3,28 @@
 /**
  * A custom ACF widget.
  */
-class ACF_Custom_Widget extends WP_Widget {
+// Register the custom widget
+function register_custom_acf_widget() {
+    register_widget('Custom_ACF_Widget');
+}
+add_action('widgets_init', 'register_custom_acf_widget');
 
-    /**
-    * Register widget with WordPress.
-    */
-    function __construct() {
+// Create the custom widget class
+class Custom_ACF_Widget extends WP_Widget {
+    public function __construct() {
         parent::__construct(
-            'acf_custom_widget', // Base ID
-            __('ACF Custom Widget', 'text_domain'), // Name
-            array( 'description' => __( 'A custom ACF widget', 'text_domain' ), 'classname' => 'acf-custom-widget' ) // Args
+            'custom_acf_widget',
+            'Custom ACF Widget',
+            array('description' => 'A custom widget to display ACF field values')
         );
     }
 
-    /**
-    * Front-end display of widget.
-    *
-    * @see WP_Widget::widget()
-    *
-    * @param array $args     Widget arguments.
-    * @param array $instance Saved values from database.
-    */
-    public function widget( $args, $instance ) {
-        echo $args['before_widget'];
-        if ( !empty($instance['title']) ) {
-            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
-        }
+    public function widget($args, $instance) {
+        // Output the ACF field value here
+        $acf_field_value = get_field('your_acf_field_name');
+        echo $acf_field_value;
 
-        echo get_field('title', 'widget_' . $args['widget_id']);
-
-
-        // BEGIN ACF CODE
-        if ( get_field('content') ) {
-            echo '<div class="content">'
-                . get_field('content') . 
-            '</div>';
-        }
-        // END ACF CODE
-
-
-        echo $args['after_widget'];
+        // You can customize the widget's HTML output as needed
     }
+}
 
-    /**
-    * Back-end widget form.
-    *
-    * @see WP_Widget::form()
-    *
-    * @param array $instance Previously saved values from database.
-    */
-    public function form( $instance ) {
-        if ( isset($instance['title']) ) {
-            $title = $instance['title'];
-        }
-    ?>
-    <p>
-      <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title' ); ?></label>
-      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-    </p>
-    <?php
-    }
-
-    /**
-    * Sanitize widget form values as they are saved.
-    *
-    * @see WP_Widget::update()
-    *
-    * @param array $new_instance Values just sent to be saved.
-    * @param array $old_instance Previously saved values from database.
-    *
-    * @return array Updated safe values to be saved.
-    */
-    public function update( $new_instance, $old_instance ) {
-        $instance = array();
-        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-
-        return $instance;
-    }
-
-} // class ACF_Custom_Widget
-
-// register ACF_Custom_Widget widget
-add_action( 'widgets_init', function(){
-  register_widget( 'ACF_Custom_Widget' );
-});
